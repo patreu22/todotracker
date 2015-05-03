41484cb0523da4e81411c04d967e26f4b1e4d81f
jmennen <janmennen@gmx.de>
2015-05-03 13:24:10 +0200

    Model migriert
    Model in die sqlite db geschrieben. 2 Beispieleinträge eingefügt

--- todolist/models.py
+++ todolist/models.py
@@ -8,4 +8,7 @@ class Todo(models.Model):
     title = models.CharField(max_length=160)
     deadline = models.DateTimeField('Deadline')
     progress = models.DecimalField(max_digits=3, decimal_places=0)
-    status = models.Booleanfield()
+    def __str__(self):              # __unicode__ on Python 2
+        return self.title
+    def status(self):
+        return progress == 100
