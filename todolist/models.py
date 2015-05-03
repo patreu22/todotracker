df11e25c59ec7e988a205d15e194cb021cc75cf2
jmennen <janmennen@gmx.de>
2015-05-03 13:10:03 +0200

    Modell hinzugefügt

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,11 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=160)
+    deadline = models.DateTimeField('Deadline')
+    progress = models.DecimalField(max_digits=3, decimal_places=0)
+    status = models.Booleanfield()
