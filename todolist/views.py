17805e2c7cb0e8b586531e5531803e9b4247d330
jmennen <janmennen@gmx.de>
2015-05-03 14:36:57 +0200

    CreateView ergänzt

--- todolist/views.py
+++ todolist/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, CreateView
 from todolist.models import Todo
 
 # Create your views here.
 class IndexView(ListView):
     template_name = 'todolist/index.html'
     model = Todo
+
+class CreateView(CreateView):
+    template_name = 'todolist/create.html'
+    model = Todo
+    fields = ['title','deadline','progress']
+    success_url = '/'
