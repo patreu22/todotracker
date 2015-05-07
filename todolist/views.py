a762e333129e175c5a7daffd1dec180e42e341c4
patreu22 <patrickreuter@outlook.com>
2015-05-07 13:03:09 +0200

    Revert "Link branch"

--- todolist/views.py
+++ todolist/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
-from django.views.generic import TemplateView, ListView, CreateView, UpdateView
+from django.views.generic import TemplateView, ListView, CreateView
 from todolist.models import Todo
 
 # Create your views here.
@@ -9,18 +9,8 @@ class IndexView(ListView):
     template_name = 'index.html'
     model = Todo
 
-class ImpressumView(TemplateView):
-    template_name = 'impressum.html'
-
-class AddView(CreateView):
-    template_name = 'add.html'
+class CreateView(CreateView):
+    template_name = 'create.html'
     model = Todo
     fields = ['title','deadline','progress']
     success_url = '/'
-
-class UpdateView(UpdateView):
-    template_name = 'edit.html'
-    model = Todo
-    fields = ['title','deadline','progress']
-    success_url = '/'
-
