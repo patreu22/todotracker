b49b98b0866cf0031607be4d2e15326aafb20fd2
jmennen <janmennen@gmx.de>
2015-05-07 12:34:36 +0200

    DateTimeField changed to DateField

--- todolist/models.py
+++ todolist/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 
 class Todo(models.Model):
     title = models.CharField(max_length=160)
-    deadline = models.DateTimeField('Deadline')
+    deadline = models.DateField('Deadline')
     progress = models.DecimalField(max_digits=3, decimal_places=0)
     def __str__(self):              # __unicode__ on Python 2
         return self.title
